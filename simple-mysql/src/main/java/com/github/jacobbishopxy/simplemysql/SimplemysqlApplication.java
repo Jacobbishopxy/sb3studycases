07176f0f89150fdaf84aef420392db48bec6dcdc
Jacob <jacobbishopxy@gmail.com>
2022-01-29 23:26:40 +0800

    feat: jpa mysql

---
+++ simple-mysql/src/main/java/com/github/jacobbishopxy/simplemysql/SimplemysqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.jacobbishopxy.simplemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemysqlApplication.class, args);
+	}
+
+}
---
+++ simple-mysql/src/test/java/com/github/jacobbishopxy/simplemysql/SimplemysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jacobbishopxy.simplemysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
